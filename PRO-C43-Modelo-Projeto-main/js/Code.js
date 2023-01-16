4db8f6a0852042f742a7bc0ee067f841a5823ba2
gabisplay <98491579+gabisplay@users.noreply.github.com>
2023-01-16 17:57:58 -0300

    Add files via upload

---
+++ PRO-C43-Modelo-Projeto-main/js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("yes/no!!", 700,170)
+    
+    fill("white");
+    textSize(15);
+    fill("lightblue");
+    text("Dica: Guarda toda a informação!", 100, 270);
+    
+// adicione código para exibir o restante dos nomes e as dicas.
+    
+
+}
